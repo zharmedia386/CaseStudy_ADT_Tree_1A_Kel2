696db62183aa5032695ad4abd170370ea0c2827c
greckminas <ranggayudhayudistira79@gmail.com>
2021-07-08 14:44:40 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,34 +2,33 @@
 #include <stdlib.h>
 
 #include "tree_pegawai.h"
-#include "tree_pegawai.c"
 
 int Menu() {
 	
 	int x = -1;
-	
+	system("cls");
 	printf (" PENDATAAN PEGAWAI PERUSAHAAN CIWARUGA CORP\n");
 	printf ("------------------------------------------------\n");
 	printf ("1. Cek Semua Data Pegawai\n");
 	printf ("2. Tambah Data Pegawai\n");
 	printf ("3. Hapus Data Pegawai\n");
 	printf ("4. Cari Data Pegawai\n");
-	printf ("0. Cari Data Pegawai\n");
+	printf ("0. Keluar Aplikasi\n");
 	
-	while (x > 4 && x<0)
-		scanf ("%d", &x);
+	scanf ("%d", &x);
+	fflush(stdin);
 	
 	return x;
 }
 
-void addPegawai(Tree *T) {
+void searchPegawai(Tree *T) {
 	infotype X;
 	address temp;
 	
 	printf ("PENDATAAN PEGAWAI PERUSAHAAN CIWARUGA CORP\n");
 	printf ("------------------------------------------------\n");
 	printf ("                 CEK DATA PEGAWAI               \n");
-	printf ("ID Pegawai : \n");
+	printf ("ID Pegawai : ");
 	scanf ("%d", &X.id);
 		
 	if(CariPegawaiDenganID(T->root, X.id)==NULL){
@@ -49,23 +48,30 @@ void removePegawai(address node, Tree T) {
 	printf ("PENDATAAN PEGAWAI PERUSAHAAN CIWARUGA CORP\n");
 	printf ("------------------------------------------------\n");
 	printf ("                 HAPUS PEGAWAI               \n");
-	printf ("ID Pegawai : \n");
+	printf ("ID Pegawai : ");
 	scanf ("%d", &X.id);
 		
 	HapusPegawai(T,&node);
 }
 
-void searchPegawai(address node) {
+void addPegawai(Tree *root) {
 	
 	infotype X;
-	
-	printf ("PENDATAAN PEGAWAI PERUSAHAAN CIWARUGA CORP\n");
-	printf ("------------------------------------------------\n");
-	printf ("                 CEK DATA PEGAWAI               \n");
-	printf ("ID Pegawai : \n");
-	scanf ("%d", &X.id);
-		
-	CariPegawaiDenganID(node, &X.id);
+	X.nama = malloc(255);
+
+    printf ("PENDATAAN PEGAWAI PERUSAHAAN CIWARUGA CORP\n");
+    printf ("--------------------------------------------\n");
+    printf ("                TAMBAH PEGAWAI               \n");
+    printf ("Nama Pegawai : ");
+    scanf ("%[^\n]", X.nama);
+    fflush(stdin);
+    printf ("ID Pegawai : ");
+    scanf ("%d", &X.id);
+    fflush(stdin);
+
+    TambahPegawai(root,Alokasi(X));
+    
+    free(X.nama);
 }
 
 
@@ -83,13 +89,18 @@ int main()
 		ch = Menu ();
 		switch (ch) {		
 			case 1: //Liat Tree
-				CetakPegawaiPreorder(P);
+				CetakPegawaiPreorder(T.root);
+				getch();
+				break;
 			case 2: //Tambah Pegawai
 				addPegawai(&T);
+				break;
 			case 3: //Delete Pegawai
 				removePegawai(P,T);
+				break;
 			case 4: //Liat Data pegawai tertentu
-				searchPegawai(P);
+				searchPegawai(&T);
+				break;
 			case 0: //Exit Aplikasi	
 				return 0;
 		}	
